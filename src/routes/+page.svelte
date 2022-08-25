0c91be44eb763babc88ebdfd57890e55b2ab7cb8
ESONHUGH <32677240+Esonhugh@users.noreply.github.com>
2022-08-25 21:47:07 +0800

    init friendlinks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,42 @@
-<h1 class="font-light text-3xl text-center">
-    Welcome to SvelteKit
+<script>
+    import PersonalCard from "./PersonalCard.svelte"
+
+    let PersonalDataArray = [];
+
+    async function GetPersonalDataJson() {
+        const res = await fetch('friends.json')
+        const text = await res.json()
+        if (res.ok) {
+            PersonalDataArray = text
+            return text
+        } else {
+            throw new Error(text)
+        }
+    }
+</script>
+
+<h1 class="font-black text-4xl text-center text-blue-700 p-2">
+    Esonhugh Blog FriendLink
 </h1>
-<p>
-    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+<p class="text-blue-500 text-center">
+    This is Esonhugh Blog Friendlink, Visit this
+    <a alt="Github" href="https://github.com/Esonhugh/Friendlink_rebuild">Github repo</a>
+    to Add your Friend link with PR.
 </p>
+<div class="p-3">
+    {#await GetPersonalDataJson()}
+        <pre class="text-center p-3"> Loading..... </pre>
+    {:then text}
+        <div class="flex-1 gap-3 grid md:grid-cols-3 ">
+            {#each PersonalDataArray as PersonalData}
+                <PersonalCard {PersonalData}/>
+            {/each}
+        </div>
+    {:catch e}
+        <pre class="text-center p-3"> {e} </pre>
+    {/await}
+</div>
+
+<footer class="text-center">
+    Esonhugh - 2022
+</footer>
---
+++ src/routes/PersonalCard.svelte
@@ -0,0 +1,33 @@
+<script>
+    export let PersonalData = {};
+    import {Card, Avatar} from '@brainandbones/skeleton';
+
+    function JumpTo(url) {
+        window.open(url, "_blank")
+    }
+</script>
+
+<Card background="bg-primary-500" class="grid md:grid-cols-2 gap-3"
+      color="text-white"
+>
+    <div class="content-center"
+         on:click={()=>JumpTo(PersonalData.url)}
+    >
+        <Avatar alt="{PersonalData.alias}"
+                background="bg-surface-500"
+                hover={false} initials="PL"
+                outlined={true} size="xl"
+                src="{PersonalData.avatar}"
+        />
+        <p class="text-white p-1 text-center"
+        >
+            {PersonalData.alias}
+        </p>
+    </div>
+    <div class="content-center"
+         on:click={()=>JumpTo(PersonalData.url)}
+    >
+        <p class="text-blue-900 text-center">{PersonalData.remark}</p>
+    </div>
+</Card>
+
